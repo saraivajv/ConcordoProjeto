2d79b9207af1511d8b31a8c9391825e6a37a5e7e
saraivajv <joaovictors2010@gmail.com>
2023-06-23 04:07:52 -0300

    Criando registro de novo usuario

--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,45 @@ using std::cin;
 
 int main(int argc, char* argv[]){
 
-    string linha;
+    string linha, textotratado;
+    int pos; int i = 0;
 
     while(true){
         getline(cin, linha);
 
         if(linha.find("create-user ") != std::string::npos){
-            cout << "Criou um usuário!!";
+
+            pos = linha.find(" ");
+            textotratado = linha.substr(pos+1, linha.find("\n"));
+
+            while(textotratado.find(" ") < textotratado.find("\n")){
+
+                if(i == 0){
+                    string login = textotratado.substr(0, linha.find(" "));
+                    cout << login << std::endl;
+                }
+                
+                if(textotratado.find(" ") != std::string::npos && i != 0){
+                    string senha = textotratado.substr(0, textotratado.find(" "));
+                    cout << senha << std::endl;
+                }
+
+                textotratado = textotratado.substr(textotratado.find(" ")+1, textotratado.find("\n"));
+                i++;
+            }
+
+            string nome = textotratado;
+            cout << nome << std::endl;
+        
+            // cout << "Criou um usuário!!" << std::endl;
         }
 
         if(linha.find("login ") != std::string::npos){
             cout << "Logado como ...";
         }
 
-        if(linha.find("quit ") != std::string::npos){
-            cout << "Saindo...";
+        if(linha.find("quit") != std::string::npos){
+            cout << "Saindo..." << std::endl;
             return 0;
         }
         
